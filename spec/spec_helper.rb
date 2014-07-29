ad118837b640ee15cd475bf430fe5980eff7dd00
Ryan Townsend <ryan@ryantownsend.co.uk>
2014-07-29 19:13:41 +0100

    Added RSpec configuration options for verifying doubles and disabling monkey patching

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,3 +5,11 @@ if ENV["CODECLIMATE_REPO_TOKEN"]
   require "codeclimate-test-reporter"
   CodeClimate::TestReporter.start
 end
+
+RSpec.configure do |config|
+  config.disable_monkey_patching!
+
+  config.mock_with :rspec do |mocks|
+    mocks.verify_doubled_constant_names = true
+  end
+end
