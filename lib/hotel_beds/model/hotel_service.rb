f593a61ebcf996652c3e5d75bc15d84f51aed1e1
Ryan Townsend <ryan@ryantownsend.co.uk>
2014-10-07 10:45:11 +0100

    Added missing attribute

--- lib/hotel_beds/model/hotel_service.rb
+++ lib/hotel_beds/model/hotel_service.rb
@@ -1,4 +1,5 @@
 require "hotel_beds/model/hotel"
+require "hotel_beds/model/supplier"
 require "hotel_beds/model/reference"
 
 module HotelBeds
@@ -12,6 +13,7 @@ module HotelBeds
       attribute :date_to, Date
       attribute :currency, String
       attribute :amount, BigDecimal
+      attribute :supplier, HotelBeds::Model::Supplier
       attribute :reference, HotelBeds::Model::Reference
     end
   end
